ca7e8d4dd05fc45c6b286dfbc3f1e1f6166bb339
Crisktina <crishidalgolopez@gmail.com>
2023-02-01 10:32:52 +0100

    Single responsability done

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<?php include_once 'vendor/autoload.php';
-require_once 'S_Single_Responsability\CoffeeShop.php';
+<?php 
+include_once 'vendor/autoload.php';
+require_once 'S_Single_Responsability/Cart.php';
+require_once 'S_Single_Responsability/Menu.php';
+require_once 'S_Single_Responsability/Products.php';
 
 
-use App\CoffeeShop;
+use App\Menu;
+use App\Cart;
+use App\Products;
 
 $products= [
     ['id'=> 1, 'name'=> 'Negro', 'price' => 2],
@@ -10,9 +15,10 @@ $products= [
     ['id'=> 3,'name'=> 'Capuccino', 'price' => 3],
 ];
 
-$shop = new CoffeeShop();
+$menu = new Menu();
 
+$productsInstance = new Products();
 
-$shop->addProducts($products);
+$productsInstance->add($products);
 
-echo $shop->viewMenu();
+echo $menu->view($productsInstance->get());
